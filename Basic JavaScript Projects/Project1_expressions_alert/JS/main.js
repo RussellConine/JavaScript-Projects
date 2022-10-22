55ba1554ddee630847e693669c8c32a24fe954e2
RussellConine <47286261+RussellConine@users.noreply.github.com>
2022-10-22 16:33:46 -0700

    Created simple JavaScript file and index.html

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,15 @@
+window.alert("Hello World!!");                                  // creates alert window displaying text
+
+var x = 'Don\'t you forget about me\nDon\'t Don\'t Don\'t Don\'t', y = 'Will you stand above me?\n Look my way, never love me'; // song lyrics using backslash to excape single quotes
+
+var Sent1 = "This is the beginning of the string";
+var Sent2 = "and this is the end of the string";
+document.write(Sent1 + " " + Sent2);                            // writes text of concatenated strings
+
+var z = 10*20;                                                  // expression multiplying two numbers; result of expression is unused
+
+function My_First_Function() {                                  // function to change button text after click 
+    var str = "This is the button text!";
+
+    document.getElementById("Button_Text").innerHTML = str;
+}

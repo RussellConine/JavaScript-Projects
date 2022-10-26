f7237c3dbafc5f2e8394b7f204026a913cc3282e
RussellConine <47286261+RussellConine@users.noreply.github.com>
2022-10-25 20:25:06 -0700

    Created js for typeof, bool, Nan, log

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,24 @@
+
+function my_Fn(){               // function to create dictionary
+    document.getElementById("Test").innerHTML = 0/0;
+}
+
+document.write(typeof "Word");
+document.write("<br>");
+document.write(typeof 3);
+document.write("<br>");
+document.write("5" + 5);
+document.write("<br>");
+document.write(isNaN("Hello"));
+document.write("<br>");
+document.write(isNaN(50));
+document.write("<br>");
+document.write(2e311);
+document.write("<br>");
+document.write(-2e311);
+document.write("<br>");
+document.write(10 > 2);
+document.write("<br>");
+document.write(10 > 12);
+
+console.log(1>2);

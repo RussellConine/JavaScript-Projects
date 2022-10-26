b2dee25c77c0ea21e08585150db12f89f1e9edd7
RussellConine <47286261+RussellConine@users.noreply.github.com>
2022-10-25 19:53:28 -0700

    Added dictionary to main.js

--- Basic JavaScript Projects/Project4_dictionaries/JS/main.js
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -1,12 +1,15 @@
 
-function my_Dictionary(){                                         // function to add 5+5 after click 
-    var car = {
+function my_Dictionary(){               // function to create dictionary
+    var car = {                         // car object contains car properties
         make: "Subaru",
         model: "Crosstrek",
         color: "white",
         drivetrain: "AWD",
         transmission: "CVT",
-        year: 2016
+        year: 2016                      // year stored as number instead of string
     };
-    document.getElementById("Dictionary").innerHTML = car.year + " " + car.make + " " + car.model;
+    
+    document.write(car.color + " " + car.year + " " + car.make + " " + car.model + " ");    // write description of car object using properties
+    delete car.color                                                                        // delete color from object
+    document.write(car.color + " " + car.year + " " + car.make + " " + car.model);          // show color as undefined since it was deleted in the previous line
 }

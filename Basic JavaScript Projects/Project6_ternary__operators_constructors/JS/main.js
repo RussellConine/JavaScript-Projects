2818dc4791a13e91f51f25733b8b11550aa77036
Russell Conine <47286261+RussellConine@users.noreply.github.com>
2022-10-27 12:33:38 -0700

    Added OOP example to main.js

--- Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -1,6 +1,19 @@
-function Vote_Function() {
-    var Age, Can_vote;
-    Age = document.getElementById("Age").value;
-    Can_vote = (Age < 18) ? "You are too young":"You are old enough";
-    document.getElementById("Vote").innerHTML = Can_vote + " to vote.";
+function Dog(name, breed, fur_type, ear_floppiness) {
+    this.name = name;
+    this.breed = breed;
+    this.fur_type = fur_type;
+    this.ear_floppiness = ear_floppiness;
+}
+
+
+function nested() {
+    document.write(Count());
+    function Count() {
+        var starting_point = 9;
+        function Plus_one() {
+            starting_point += 1;
+        }
+        Plus_one();
+        return starting_point;
+    }
 }
